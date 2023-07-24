2d3f02ca9b54d517a2361e9bd437da1a8abe0a18
ZorAnderius <gabriel.zorander@gmail.com>
2023-07-24 19:12:57 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
