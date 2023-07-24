815df569747300ec6687661290d10eeeb083e51f
ZorAnderius <gabriel.zorander@gmail.com>
2023-07-24 19:27:26 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
