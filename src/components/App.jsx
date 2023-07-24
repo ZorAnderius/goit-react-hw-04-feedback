7a5d3ab454075d0b87d09c1b5ad59ec6e42b1dd7
ZorAnderius <gabriel.zorander@gmail.com>
2023-07-24 19:11:28 +0100

    delete useless files and add hooks (refactoring code form class style to hooks style)  for App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
@@ -9,76 +9,86 @@ import appCSS from './App.module.css';
 
 const shortid = require('shortid');
 
-export class App extends Component {
-  state = { good: 0, neutral: 0, bad: 0 };
+const getRandomID = () => {
+  const id = shortid.generate();
+  return id;
+};
 
-  #getRandomID = () => {
-    const id = shortid.generate();
-    return id;
+const getArrayID = arr => {
+  const arr_id = arr.map(() => getRandomID());
+  return arr_id;
+};
+
+const getIDAndDataObj = (id, data) => {
+  return id.map((id, index) => {
+    return { id, data: data[index] };
+  });
+};
+
+const countTotalFeedback = ({ good, neutral, bad }) => {
+  return good + neutral + bad;
+};
+
+const countPositiveFeedbackPercentage = ({ good, neutral, bad }) => {
+  return Math.round((100 * good) / countTotalFeedback({ good, neutral, bad }));
+};
+
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onChooseFeedbackBtn = e => {
+    if (e === 'good') {
+      setGood(state => state + 1);
+    }
+    if (e === 'neutral') {
+      setNeutral(state => state + 1);
+    }
+    if (e === 'bad') {
+      setBad(state => state + 1);
+    }
   };
 
-  #getArrayID = arr => {
-    const arr_id = arr.map(() => this.#getRandomID());
-    return arr_id;
-  };
+  const btnTxt = ['good', 'neutral', 'bad'];
+  const btnData = getIDAndDataObj(getArrayID(btnTxt), btnTxt);
+  const totalFeedback = countTotalFeedback({ good, neutral, bad });
+  const positiveFeedback = countPositiveFeedbackPercentage({
+    good,
+    neutral,
+    bad,
+  });
 
-  #getIDAndDataObj = (id, data) => {
-    return id.map((id, index) => {
-      return { id, data: data[index] };
-    });
-  };
-
-  #countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-
-  #countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    return Math.round((100 * good) / this.#countTotalFeedback());
-  };
-
-  onChooseFeedbackBtn = e => {
-    this.setState(prevState => ({ [e]: prevState[e] + 1 }));
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const btnTxt = Object.keys(this.state);
-    const btnData = this.#getIDAndDataObj(this.#getArrayID(btnTxt), btnTxt);
-    const totalFeedback = this.#countTotalFeedback();
-    const positiveFeedback = this.#countPositiveFeedbackPercentage();
-
-    return (
-      <div className={appCSS.main_container}>
-        <Section
-          title={'Please leave feedback'}
-          title_style={'header-title'}
-          section_style={'feedback-section-container'}
-        >
-          <FeedbackOptions
-            options={btnData}
-            onLeaveFeedback={this.onChooseFeedbackBtn}
+  return (
+    <div className={appCSS.main_container}>
+      <Section
+        title={'Please leave feedback'}
+        title_style={'header-title'}
+        section_style={'feedback-section-container'}
+      >
+        <FeedbackOptions
+          options={btnData}
+          onLeaveFeedback={onChooseFeedbackBtn}
+        />
+      </Section>
+      <Section
+        title={'Statistics'}
+        title_style={'statistics-title'}
+        section_style={'stat-section-container'}
+      >
+        {totalFeedback ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positiveFeedback}
           />
-        </Section>
-        <Section
-          title={'Statistics'}
-          title_style={'statistics-title'}
-          section_style={'stat-section-container'}
-        >
-          {totalFeedback ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalFeedback}
-              positivePercentage={positiveFeedback}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+      ;
+    </div>
+  );
+};
